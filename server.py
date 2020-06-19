474d3899a8a03397eb4e42151932970bf3d77830
Kane Hollingsworth <kane.hollingsworth@yahoo.com>
2020-06-19 00:31:03 -0500

    This is the first update to the server. The server is capable of having one user whos name is logger with a hashed password. This password is hashed so it cannot be seen by people that are listening on the network.

--- server.py
+++ server.py
@@ -1,15 +1,62 @@
-#credits go to https://pypi.org/project/pyftpdlib/ atm...
-#will add to this
-from pyftpdlib.authorizers import DummyAuthorizer
+# credits go to https://pyftpdlib.readthedocs.io/en/latest/tutorial.html atm...
+
+import os
+import sys
+import logging
+from hashlib import md5
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
+from pyftpdlib.authorizers import DummyAuthorizer, AuthenticationFailed
 
-authorizer = DummyAuthorizer()
-authorizer.add_user("user", "12345", "C:/Users/18503", perm="elradfmwMT")
-authorizer.add_anonymous("/home/nobody")
+# creation of the dummy authorizer to be formatted with md5 so the password is not exposed in case if
+# someone is listening
 
+class DummyMD5Authorizer(DummyAuthorizer):
+
+    def validate_authentication(self, username, password, handler):
+        if sys.version_info >= (3, 0):
+            password = md5(password.encode('latin1'))
+        hash = md5(password).hexdigest()
+        try:
+            if self.user_table[username]['pwd'] != hash:
+                raise KeyError
+        except KeyError:
+            raise AuthenticationFailed
+
+
+# Generating a hash of the password
+hash = md5('9lGi^W0rjlJ5Ve').hexdigest()
+
+# Instantiate an md5 dummy authorizer for managing 'virtual' users
+authorizer = DummyMD5Authorizer()
+
+# Define a new user having only write and make directory permissions
+# adding a user that can only make a directory and write to the directories that it creates
+authorizer.add_user("logger", hash, str(os.getcwd()), perm="mw")
+
+# Instantiate FTP handler class
 handler = FTPHandler
 handler.authorizer = authorizer
 
-server = FTPServer(("127.0.0.1", 21), handler)
+# Define a customized banner (string returned when client connects)
+handler.banner = "pyftpdlib based ftpd ready."
+
+# Specify a masquerade address and the range of ports to use for
+# passive connections.  Decomment in case you're behind a NAT.
+#handler.masquerade_address = '151.25.42.11'
+#handler.passive_ports = range(60000, 65535)
+
+# Instantiate FTP server class and listen on 0.0.0.0:2121
+address = ('', 2121)
+server = FTPServer(address, handler)
+
+# set a limit for connections
+server.max_cons = 256
+server.max_cons_per_ip = 5
+
+# adding a logging location for all logs to be stored
+logging.basicConfig(filename='/var/log/pyftpd.log', level=logging.INFO)
+
+# start ftp server
 server.serve_forever()
+
